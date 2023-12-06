1ad769e0eec79f692aa4881f904cf09dc12a7561
NaumDeveloper <vov4.games@yandex.ru>
2023-12-07 01:25:54 +0200

    corrected

--- app/Http/Controllers/User/IndexController.php
+++ app/Http/Controllers/User/IndexController.php
@@ -3,16 +3,34 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-  public function index(){
+    /**
+     * Handle the incoming request.
+     *
+     * @return Application|Factory|View
+     */
+
+    /*
+    public function __invoke(Request $request) {
+        return view('user.index');
+    }
+    */
+
+    public function index(){
 
       //return view('user.index');
       return view('layout.site');
 
-
   }
+
+
+
 
 
 }
